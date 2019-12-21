5f33bfe61e47d75551e906202161c2f9d5165a7c
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-12-21 03:02:52 +0100

    Finish first cli version

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,60 @@
 #!/usr/bin/env node
 
-console.log("Hello world");
+import yargs from "yargs";
+import * as provider from "./provider";
+
+async function queryDataSpanTypes(): Promise<any> {
+  const dataSpans = await provider.fetchDataSpans();
+
+  dataSpans.forEach(value => {
+    console.log(value.dataSpanId);
+  });
+}
+
+async function queryRanking(dataSpans: Array<string>): Promise<any> {
+  if (!dataSpans.includes("all")) {
+    const allowedDataSpans = await provider.fetchDataSpans();
+    const allowedDataSpanIds = allowedDataSpans.map(
+      dataSpan => dataSpan.dataSpanId
+    );
+
+    dataSpans.forEach(async dataSpan => {
+      if (allowedDataSpanIds.includes(dataSpan)) {
+        const ranking = await provider.fetchRanking(dataSpan);
+        console.log(JSON.stringify(ranking));
+      } else {
+        console.error(
+          `'${dataSpan}' is not a valid data span, run 'distrowatch list-types'`
+        );
+      }
+    });
+  } else {
+    const allRankings = await provider.fetchAllRankings();
+    console.log(JSON.stringify(allRankings));
+  }
+}
+
+yargs
+  .command(
+    ["list-types", "list-spans", "list"],
+    "list the types of data spans you can choose from",
+    () => {},
+    () => {
+      queryDataSpanTypes();
+    }
+  )
+  .command(
+    ["fetch-ranking [data-spans..]", "get-ranking", "ranking"],
+    "fetches the ranking",
+    yargs => {
+      yargs.positional("data-spans", {
+        describe: "the data span(s) to fetch",
+        default: ["all"]
+      });
+    },
+    argv => {
+      queryRanking(argv.dataSpans as Array<string>);
+    }
+  )
+  .demandCommand()
+  .wrap(yargs.terminalWidth()).argv;
