5f217ff1fab184c981e9ef7445b67b9d435321d3
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-12-26 20:33:08 +0100

    Improve cli output

--- src/cli.ts
+++ src/cli.ts
@@ -1,32 +1,87 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs';
-import * as provider from './provider';
+import fs from 'fs';
+import path from 'path';
+import { fetchRanking, fetchDataSpans, fetchAllRankings, DistroWatchRankingType } from './index';
 
-async function queryDataSpanTypes(): Promise<any> {
-  const dataSpans = await provider.fetchDataSpans();
-
-  dataSpans.forEach(value => {
-    console.log(`${value.dataSpanId.padEnd(15)} ${value.dataSpanName}`);
-  });
+interface ListTypesCommandArguments {
+  file: string;
+  json: boolean;
 }
 
-async function queryRanking(dataSpans: Array<string>): Promise<any> {
-  if (!dataSpans.includes('all')) {
-    const allowedDataSpans = await provider.fetchDataSpans();
-    const allowedDataSpanIds = allowedDataSpans.map(dataSpan => dataSpan.dataSpanId);
+interface FetchRankingCommandArguments {
+  dataSpans: Array<string>;
+  file: string;
+  json: boolean;
+}
 
-    dataSpans.forEach(async dataSpan => {
-      if (allowedDataSpanIds.includes(dataSpan)) {
-        const ranking = await provider.fetchRanking(dataSpan);
-        console.log(JSON.stringify(ranking));
-      } else {
-        console.error(`'${dataSpan}' is not a valid data span, run 'distrowatch list-types'`);
-      }
+async function handleListTypesCommand(args: ListTypesCommandArguments) {
+  const dataSpans = await fetchDataSpans();
+
+  if (args.file) {
+    const outputFile = path.resolve(__dirname, args.file);
+    fs.writeFileSync(outputFile, JSON.stringify(dataSpans));
+  }
+
+  if (!args.json) {
+    const maxIdLength = Math.max(...dataSpans.map(dataSpan => dataSpan.dataSpanId.length));
+    const idEndPadding = Math.max(maxIdLength + 2, 15);
+    console.log(`${'Span Id'.padEnd(idEndPadding)} Span Name`);
+    dataSpans.forEach(value => {
+      console.log(`${value.dataSpanId.padEnd(idEndPadding)} ${value.dataSpanName}`);
     });
   } else {
-    const allRankings = await provider.fetchAllRankings();
-    console.log(JSON.stringify(allRankings));
+    console.log(JSON.stringify(dataSpans));
+  }
+}
+
+async function fetchRankingBySpans(dataSpans: Array<string>) {
+  if (dataSpans.includes('all')) {
+    const allRankings = await fetchAllRankings();
+    return allRankings;
+  }
+
+  const allowedDataSpans = await fetchDataSpans();
+  const allowedDataSpanIds = allowedDataSpans.map(dataSpan => dataSpan.dataSpanId);
+
+  return Promise.all(
+    dataSpans.map(async dataSpan => {
+      if (allowedDataSpanIds.includes(dataSpan)) {
+        const ranking = await fetchRanking(dataSpan);
+        return ranking;
+      } else {
+        throw new Error(`'${dataSpan}' is not a valid data span`);
+      }
+    }),
+  );
+}
+
+async function handleFetchRankingCommand(args: FetchRankingCommandArguments) {
+  const rankings = await fetchRankingBySpans(args.dataSpans);
+
+  if (args.file) {
+    const outputFile = path.resolve(__dirname, args.file);
+    fs.writeFileSync(outputFile, JSON.stringify(rankings));
+  }
+
+  if (!args.json) {
+    console.log(
+      `${'Span Name'.padEnd(25)} ${'Ranking Type'.padEnd(20)} ` +
+        `${'Name'.padEnd(20)} ${'Rank'.padEnd(5)} ${'Value'.padEnd(8)} ${'URL (before redirects)'}`,
+    );
+    rankings.forEach(ranking => {
+      const mappedRankingType = DistroWatchRankingType[ranking.rankingType];
+      ranking.distributionsRanking.forEach(distribution => {
+        console.log(
+          `${ranking.dataSpanName.padEnd(25)} ${mappedRankingType.padEnd(20)} ` +
+            `${distribution.name.padEnd(20)} ${distribution.rank.toString().padEnd(5)} ` +
+            `${distribution.value.toString().padEnd(8)} ${distribution.url}`,
+        );
+      });
+    });
+  } else {
+    console.log(JSON.stringify(rankings));
   }
 }
 
@@ -34,23 +89,46 @@ yargs
   .command(
     ['list-types', 'list-spans', 'list'],
     'list the types of data spans you can choose from',
-    () => {},
-    () => {
-      queryDataSpanTypes();
+    yargs => {
+      yargs
+        .option('file', {
+          description: 'also write output in json format to a file',
+          alias: 'f',
+          type: 'string',
+        })
+        .option('json', {
+          description: 'print json instead of a table',
+          alias: 'j',
+          type: 'boolean',
+        });
+    },
+    argv => {
+      handleListTypesCommand((argv as unknown) as ListTypesCommandArguments);
     },
   )
   .command(
     ['fetch-ranking [data-spans..]', 'get-ranking', 'ranking'],
-    'fetches the ranking',
+    'prints out the ranking data in the specified data span(s)',
     yargs => {
-      yargs.positional('data-spans', {
-        describe: 'the data span(s) to fetch',
-        type: 'string',
-        default: ['all'],
-      });
+      yargs
+        .positional('data-spans', {
+          description: 'the data span(s) to fetch',
+          type: 'string',
+          default: ['all'],
+        })
+        .option('file', {
+          description: 'also write output in json format to a file',
+          alias: 'f',
+          type: 'string',
+        })
+        .option('json', {
+          description: 'print json instead of a table',
+          alias: 'j',
+          type: 'boolean',
+        });
     },
     argv => {
-      queryRanking(argv.dataSpans as Array<string>);
+      handleFetchRankingCommand((argv as unknown) as FetchRankingCommandArguments);
     },
   )
   .demandCommand(1, '')
