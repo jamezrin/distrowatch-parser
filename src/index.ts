503eaea35de3cdbc2d0d81b88c37ee418c4a6a44
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2021-03-19 21:15:07 +0100

    Use random user agents

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
 
+const sampleUserAgents = [
+  'Mozilla/5.0 (Windows NT 10.; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+  'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+  'Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T550 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.3 Chrome/38.0.2125.102 Safari/537.36',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+  'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+  'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+  'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)',
+];
+
 function createPagePath(dataSpanId?: string): string {
   return dataSpanId
     ? `https://distrowatch.com/?language=EN&dataspan=${dataSpanId}`
@@ -9,7 +21,16 @@ function createPagePath(dataSpanId?: string): string {
 
 async function makeRankingRequest(dataSpan?: string): Promise<AxiosResponse> {
   const pagePath = createPagePath(dataSpan);
-  const response = await axios.get(pagePath);
+
+  const randomIndex = Math.floor(Math.random() * sampleUserAgents.length);
+  const randomUserAgent = sampleUserAgents[randomIndex];
+
+  const response = await axios.get(pagePath, {
+    headers: {
+      'User-Agent': randomUserAgent,
+    },
+  });
+
   return response;
 }
 
